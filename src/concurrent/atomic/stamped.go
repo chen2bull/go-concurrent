74fb0c2867279cfa9e2af1145d35946b2263921f
chenmingjian <cmj151531@alibaba-inc.com>
2019-06-13 14:40:42 +0800

    stamped_test

--- src/concurrent/atomic/stamped.go
+++ src/concurrent/atomic/stamped.go
@@ -1,4 +1,4 @@
-package util
+package atomic
 
 import (
 	"sync/atomic"
@@ -11,36 +11,39 @@ type pair struct {
 }
 
 type StampedReference struct {
-	p unsafe.Pointer
+	p *unsafe.Pointer
 }
 
-func NewStampedReference(v interface{}, stamped int64) StampedReference {
+func NewStampedReference(v interface{}, stamped int64) *StampedReference {
 	var p = unsafe.Pointer(&pair{v: v, stamped: stamped})
-	return StampedReference{p: p}
+	return &StampedReference{p: &p}
 }
 
 func (sr *StampedReference) GetReference() interface{} {
-	var a = (*pair)(atomic.LoadPointer(&sr.p))
+	var a = (*pair)(atomic.LoadPointer(sr.p))
 	return a.v
 }
 
 func (sr *StampedReference) GetStamp() interface{} {
-	var a = (*pair)(atomic.LoadPointer(&sr.p))
+	var a = (*pair)(atomic.LoadPointer(sr.p))
 	return a.stamped
 }
 
 func (sr *StampedReference) Get() (interface{}, int64) {
-	var a = (*pair)(atomic.LoadPointer(&sr.p))
+	var a = (*pair)(atomic.LoadPointer(sr.p))
 	return a.v, a.stamped
 }
 
 func (sr *StampedReference) CompareAndSet(expectedV interface{}, newV interface{}, expectedStamp int64, newStamp int64) bool {
-	var cur = (*pair)(atomic.LoadPointer(&sr.p))
+	// 惯用方式:先把old的指针值读出来,做一系列判断以后,改变的时候,用old值作为基准
+	var old = atomic.LoadPointer(sr.p)
+	var cur = (*pair)(old)
+
 	if cur.v == expectedV && cur.stamped == expectedStamp {
 		if cur.v != newV || cur.stamped != newStamp {
-			return atomic.CompareAndSwapPointer(&sr.p, unsafe.Pointer(&cur), unsafe.Pointer(&pair{v: newV, stamped: newStamp}))
+			//fmt.Printf("*sr.p:%v old:%v\n", *sr.p, old)
+			return atomic.CompareAndSwapPointer(sr.p, old, unsafe.Pointer(&pair{v: newV, stamped: newStamp}))
 		}
-		return true
 	}
 	return false
 }
