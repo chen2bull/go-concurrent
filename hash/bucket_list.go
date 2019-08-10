d03acc157a9c7d4d2c732c527a38d2363fd618bf
chenmingjian <cmj151531@alibaba-inc.com>
2019-08-10 18:36:44 +0800

    bucket_list

--- hash/bucket_list.go
+++ hash/bucket_list.go
@@ -2,9 +2,17 @@ package hash
 
 import (
 	"github.com/cmingjian/go-concurrent/atomic"
+	"math"
 )
 
 type BucketList struct {
+	head *bucketListNode
+}
+
+func NewBucketList() *BucketList {
+	head := newBucketListSentinelNode(0)
+	head.next = atomic.NewMarkableReference(newBucketListSentinelNode(math.MaxInt32), false)
+	return &BucketList{head: head}
 }
 
 type bucketListNode struct {
@@ -23,7 +31,7 @@ func newBucketListSentinelNode(key int) *bucketListNode { // sentinel constructo
 	return &bucketListNode{key: key, next: next}
 }
 
-func (node *bucketListNode) getNext() interface{} {
+func (node *bucketListNode) getNext() *bucketListNode {
 	var currMarked bool
 	entryRef, currMarked := node.next.Get()
 	for ; currMarked; { // 如果marked表示该元素已经删除了,既然已标记删除,那么value肯定不会为nil
@@ -32,5 +40,17 @@ func (node *bucketListNode) getNext() interface{} {
 		node.next.CompareAndSet(entryRef, succRef, true, succMarked)
 		entryRef, currMarked = node.next.Get()
 	}
-	return entryRef
+	entry := entryRef.(bucketListNode)
+	return &entry
+}
+
+// 只能用与head
+func (node *bucketListNode) find(key int) (*bucketListNode, *bucketListNode) {
+	pred := node
+	curr := pred.getNext()
+	for ; curr.key < key; {	// 列表有值为0和math.MaxInt32的哨兵
+		pred = curr
+		curr = pred.getNext()
+	}
+	return pred, curr
 }
