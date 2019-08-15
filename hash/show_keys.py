d16475f5571a13be59d0178f21126062319e943a
chenmingjian <cmj151531@alibaba-inc.com>
2019-08-15 19:22:48 +0800

    lock free map:sentinal keys calc

--- hash/show_keys.py
+++ hash/show_keys.py
@@ -32,7 +32,11 @@ bucket array长度为2^N的时候,下标0,1,2,3,...,(2^N-1)对应的calc_key(inp
 因此，bucket array一种比较合适的增长方式是，长度每次都变成原来的2倍。key值为calc_key(index)的‘哨兵’，正好等分哈希表。
 并且，bucket array长度由2^n变成2^(n+1)的时候，bucket array下标为0～(2^n-1)的哈希值不需要重算！！！
 
+只要bucket array的容量不大于2^23，那么calc_key(input_value)返回值的最低1位就不会为1。因此，只要限制bucket_list的
+大小不超过2^23（对于哈希表来说，这是非常大的一个值）
 实现无锁哈希表的时候，即使bucket array不按“长度每次都变成原来的2倍”的方式来增长的话，无锁哈希表也可以正常地工作。
+
+
 """
 
 
@@ -52,9 +56,28 @@ def calc_key(input_val):
     return "{0:24b}".format(reverse(input_val))
 
 
+def calc_parent(input_val, bucket_size):
+    parent = bucket_size
+    while True:
+        parent = parent >> 1
+        if parent <= input_val:
+            break
+    parent = input_val - parent
+    return parent
+
+
 def main():
+    print("===============================")
     for input_val in range(100):
         print(calc_key(input_val))
+    print("===============================")
+    for input_val in range(15):
+        index = input_val
+        parent = calc_parent(input_val, 16)
+        index_key = calc_key(index)
+        parent_key = calc_key(parent)
+        print("index:{0:4b}({0:d}) parent:{1:4b}({1:d}) index_key:{2:s} parent_key:{3:s}".format(
+            input_val, parent, index_key, parent_key))
 
 
 if __name__ == '__main__':
