181e586a91081cfefa37d9bfb4937e81c462430a
chenmingjian <cmj151531@alibaba-inc.com>
2019-09-06 19:34:15 +0800

    list

--- hash/list.go
+++
@@ -1,5 +0,0 @@
-package hash
-
-type node struct {
-
-}
--- list/set.go
+++
@@ -1,7 +0,0 @@
-package list
-
-type Set interface {
-	Add(v interface{}) bool
-	Remove(v interface{}) bool
-	contains(v interface{}) bool
-}
